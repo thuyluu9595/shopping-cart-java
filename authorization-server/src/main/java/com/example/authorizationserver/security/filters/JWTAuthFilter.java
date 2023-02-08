23d7668680a153f2b4cdd8bf978ca66dc69e1208
thuyluu <thuyluu9595@gmail.com>
2023-02-08 14:03:01 -0800

    implemented JWTAuthFilter

--- authorization-server/src/main/java/com/example/authorizationserver/security/filters/JWTAuthFilter.java
+++ authorization-server/src/main/java/com/example/authorizationserver/security/filters/JWTAuthFilter.java
@@ -1,15 +1,37 @@
 package com.example.authorizationserver.security.filters;
 
+import com.example.authorizationserver.model.JWTAuthModel;
+import com.example.authorizationserver.service.redis.TokensRedisService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JWTAuthFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
 
     private ObjectMapper mapper = new ObjectMapper();
+
+    private final TokensRedisService tokensRedisService;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            JWTAuthModel authModel = mapper.readValue(request.getInputStream(), JWTAuthModel.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(authModel.getEmail(), authModel.getPassword());
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
