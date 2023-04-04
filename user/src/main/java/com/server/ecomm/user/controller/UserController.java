55d20dc3fbd68d16a5b26b49a06ef7c98699e825
thuyluu <thuyluu9595@gmail.com>
2023-04-04 01:03:59 -0700

    Cleaned code for UserController

--- user/src/main/java/com/server/ecomm/user/controller/UserController.java
+++ user/src/main/java/com/server/ecomm/user/controller/UserController.java
@@ -4,7 +4,6 @@ import com.server.ecomm.user.config.proxy.AuthServiceProxy;
 import com.server.ecomm.user.entity.User;
 import com.server.ecomm.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
